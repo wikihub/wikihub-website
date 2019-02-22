8f123a618325334e97620b2ade6b18a9dc31438b
Behzad Samadi <behzad.samadi@gmail.com>
2019-02-22 09:01:50 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 npm install
 rm -rf public
-git clone --depth 1 https://bsamadi:$GITHUB_TOKEN@$DEPLOY_REPO $DEPLOY_DIR
+git clone --depth 1 https://$GITHUB_USER:$GITHUB_TOKEN@$DEPLOY_REPO $DEPLOY_DIR
 hugo --config config.toml
 cd $DEPLOY_DIR
-git config user.email "behzad.samadi@gmail.com"
-git config --global user.name "bsamadi"
+git config user.email "$GITHUB_EMAIL"
+git config --global user.name "$GITHUB_USER"
 git add -A
 git commit -m "Updated by Travis CI"
 git push origin master
