c66138d6f4b2092c0e4ac94f238ce39cd027caa4
Behzad Samadi <behzad.samadi@gmail.com>
2019-02-22 00:37:37 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 npm install
 rm -rf public
-git clone --depth 1 https://wikihub:$GITHUB_ACCESS_TOKEN@github.com/wikihub/wikihub.github.io.git public
+git clone --depth 1 https://bsamadi:$GITHUB_ACCESS_TOKEN@github.com/wikihub/wikihub.github.io.git public
 hugo --config config.toml
 cd public
 git config user.email "behzad.samadi@gmail.com"
