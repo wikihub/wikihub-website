2b2a493e282fe4a9a9635125d58dd1e8b949272a
Behzad Samadi <behzad.samadi@gmail.com>
2019-02-21 23:46:36 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ rm -rf public
 git clone --depth 1 https://bsamadi:$GITHUB_ACCESS_TOKEN@github.com/wikihub/wikihub.github.io.git public
 hugo --config config.toml
 cd public
-git config user.email "nubonetics@gmail.com"
-git config --global user.name "nubonetics"
+git config user.email "behzad.samadi@gmail.com"
+git config --global user.name "bsamadi"
 git add -A
 git commit -m "Updated by Travis CI"
 git push
