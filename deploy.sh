f67477d0014f75a48f46cf86c8ba3cddb5b24e41
Behzad Samadi <behzad.samadi@gmail.com>
2019-02-21 23:40:51 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 npm install
 rm -rf public
-git clone --depth 1 https://nubonetics:$GITHUB_ACCESS_TOKEN@github.com/agritech-robotics/agritech-robotics.github.io.git public
+git clone --depth 1 https://bsamadi:$GITHUB_ACCESS_TOKEN@github.com/wikihub/wikihub.github.io.git public
 hugo --config config.toml
 cd public
 git config user.email "nubonetics@gmail.com"
 git config --global user.name "nubonetics"
 git add -A
-git commit -m "Build from $CI_SERVER_NAME $CI_PIPELINE_ID"
+git commit -m "Updated by Travis CI"
 git push
